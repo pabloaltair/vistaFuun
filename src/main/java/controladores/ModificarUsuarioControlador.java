bdc1e1c58792216c0d67041481d2f420d41831b2
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-05-27 20:53:25 +0200

    Muestra mensaje exitoso de modificacion

--- src/main/java/controladores/ModificarUsuarioControlador.java
+++ src/main/java/controladores/ModificarUsuarioControlador.java
@@ -48,18 +48,19 @@ public class ModificarUsuarioControlador extends HttpServlet {
 
             // Detectar si el resultado es un error para asignar el mensaje correcto
             if (resultado.toLowerCase().contains("error") || resultado.toLowerCase().contains("código de respuesta")) {
-                request.setAttribute("errorMensaje", resultado);
+                request.setAttribute("errorMessage", resultado);   // cambia errorMensaje por errorMessage
             } else {
-                request.setAttribute("successMensaje", resultado);
+                request.setAttribute("successMessage", resultado); // cambia successMensaje por successMessage
             }
 
         } catch (NumberFormatException e) {
             e.printStackTrace();
-            request.setAttribute("errorMensaje", "ID de usuario inválido.");
+            request.setAttribute("errorMessage", "ID de usuario inválido.");
         } catch (Exception e) {
             e.printStackTrace();
-            request.setAttribute("errorMensaje", "Error al modificar el usuario: " + e.getMessage());
+            request.setAttribute("errorMessage", "Error al modificar el usuario: " + e.getMessage());
         }
+
 
         request.getRequestDispatcher("menuAdministrador.jsp").forward(request, response);
     }
