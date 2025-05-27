8e6afa0e923bae7c4eaaa52f7989d505743eaba3
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-05-27 22:34:27 +0200

    Java Doc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controladores"},{"l":"Dtos"},{"l":"Servicios"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controladores"},{"l":"dtos"},{"l":"servicios"},{"l":"utilidades"}];updateSearchResults();
